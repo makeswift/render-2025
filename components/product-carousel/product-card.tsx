ee27cbd8155796f610c0804e47e6200103fdae15
Nathan Booker <nathan.booker@bigcommerce.com>
2024-06-11 10:52:42 -0400

    Prefetch high-priority links

--- components/product-carousel/product-card.tsx
+++ components/product-carousel/product-card.tsx
@@ -110,6 +110,7 @@ export const ProductCard = ({
         <ProductCardInfoProductName>
           {product.path ? (
             <Link
+              prefetch={true}
               className="focus:outline focus:outline-4 focus:outline-offset-2 focus:outline-blue-primary/20 focus:ring-0"
               href={product.path}
             >
