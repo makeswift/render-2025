c56d87ccb54f89c1e93ab6a03f71ef0b5c32ef18
Nathan Booker <nathan.booker@bigcommerce.com>
2024-06-11 20:26:26 -0400

    Bump products per page to 50

--- app/(default)/(faceted)/fetch-faceted-search.ts
+++ app/(default)/(faceted)/fetch-faceted-search.ts
@@ -152,7 +152,7 @@ export const PublicToPrivateParams = PublicSearchParamsSchema.catchall(SearchPar
 export const fetchFacetedSearch = cache(
   // We need to make sure the reference passed into this function is the same if we want it to be memoized.
   async (params: z.input<typeof PublicSearchParamsSchema>) => {
-    const { after, limit = 9, sort, filters } = PublicToPrivateParams.parse(params);
+    const { after, limit = 50, sort, filters } = PublicToPrivateParams.parse(params);
 
     return getProductSearchResults({
       after,
